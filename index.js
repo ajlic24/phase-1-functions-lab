3df78f3ccd1e07a206ababb2a5b388ae4a980ec3
ajlic24@gmail.com <ajlic24@gmail.com>
2021-11-23 19:11:00 -0500

    commit tree 7b638073d7837b4c1f03afe68a29271634cc1517

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400){
+        return 0;
+    } else if (feet <= 2000){
+        return (feet - 400) * .02;
+    } else if (feet > 2500){
+        return `cannot travel that far`;
+    } else if (feet > 2000) {
+        return 25;
+    } 
+}
